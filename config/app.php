d45488951c0e55c3235f5811ac96b9212b78048b
testoodoo1 <test.oodoo1@gmail.com>
2018-08-18 00:26:49 +0530

    Dacastro4 providers & aliases added

--- config/app.php
+++ config/app.php
@@ -159,6 +159,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Dacastro4\LaravelGmail\LaravelGmailServiceProvider::class,
 
     ],
 
@@ -208,6 +209,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'LaravelGmail' => Dacastro4\LaravelGmail\Facades\LaravelGmail::class,
 
     ],
 
