be359dd24e8b124991eaaca713bba1abbdbeeadd
testoodoo1 <test.oodoo1@gmail.com>
2018-09-03 20:33:07 +0530

    gmail api added

--- app/Http/Controllers/GmailController.php
+++ app/Http/Controllers/GmailController.php
@@ -8,8 +8,8 @@ use LaravelGmail;
 class GmailController extends Controller
 {
     public function inbox(){
-    	var_dump("hi");
 		$messages = LaravelGmail::message()->unread()->preload()->all();
+		var_dump($messages); die;	
 		foreach ( $messages as $message ) {
 		    $body = $message->getHtmlBody();
 		    $subject = $message->getSubject();
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,23 @@
 |
 */
 
+
+Route::get('/oauth/gmail', function (){
+    return LaravelGmail::redirect();
+});
+
+Route::get('/oauth/gmail/callback', function (){
+    LaravelGmail::makeToken();
+    return redirect()->to('/');
+});
+
+Route::get('/oauth/gmail/logout', function (){
+    LaravelGmail::logout(); //It returns exception if fails
+    return redirect()->to('/');
+});
+
+
+
 Route::get('/', function () {
     return view('welcome');
 });
